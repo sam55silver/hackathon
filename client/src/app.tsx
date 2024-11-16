df9803dbde5a7d84e00180c1fc935ed5483f196a
Sam Silver <sam55silver@gmail.com>
2024-11-16 14:37:55 -0400

    add file downloading

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,72 +1,96 @@
 import { Button } from './components/ui/button';
-import { MapPin } from 'lucide-react';
+import { File } from 'lucide-react';
 import WindSVG from './assets/wind-turbines-icon.svg';
 import React from 'react';
+import { Textarea } from './components/ui/textarea';
+
+enum MessageType {
+  user = 'User',
+  agent = 'Agent',
+}
+
+interface FileType {
+  name: string;
+  link: string;
+}
+
+interface Message {
+  type: MessageType;
+  content: string;
+  files: null | FileType[];
+}
 
 function App() {
-  const [location, setLocation] = React.useState('');
-  const [socket, setSocket] = React.useState<WebSocket | null>(null);
-  const [messages, setMessages] = React.useState<string[]>([]);
-
-  React.useEffect(() => {
-    const ws = new WebSocket('ws://localhost:8000/ws');
-
-    ws.onopen = () => {
-      console.log('WebSocket connected');
-    };
-
-    ws.onmessage = (event) => {
-      // Handle incoming messages
-      setMessages((prevMessages: string[]) => [...prevMessages, event.data]);
-    };
-
-    ws.onclose = () => {
-      console.log('WebSocket disconnected');
-    };
-
-    ws.onerror = (error) => {
-      console.error('WebSocket error:', error);
-    };
-
-    setSocket(ws);
-
-    // Cleanup on unmount
-    return () => {
-      ws.close();
-    };
-  }, []);
-
-  const sendMessage = () => {
-    if (socket && location) {
-      socket.send(location);
-      setLocation('');
-    }
-  };
+  const [query, setQuery] = React.useState('');
+  const [messages, setMessages] = React.useState<Message[]>([]);
 
   const handleInputChange = (event: any) => {
-    setLocation(event.target.value); // Update state with input's value
+    setQuery(event.target.value); // Update state with input's value
+  };
+
+  const sendMessage = () => {
+    const userMsg = { type: MessageType.user, content: query, files: null };
+    setMessages([...messages, userMsg]);
+    setQuery('');
+
+    fetch('http://localhost:8000/query_agent', {
+      method: 'POST',
+      body: JSON.stringify({ query }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((data) => data.json())
+      .then((data) =>
+        setMessages([
+          ...messages,
+          userMsg,
+          { type: MessageType.agent, content: data.content, files: data.files },
+        ])
+      );
   };
 
   return (
-    <main className="font-geist text-black h-dvh w-full flex flex-col justify-center items-center gap-8">
-      <img className="w-48" src={WindSVG} />
-      <h1 className="text-4xl text-bold">Onshore Wind Development</h1>
-      <div className="flex gap-2 items-center h-14 w-1/2 rounded-2xl border border-black bg-background px-4 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">
-        <MapPin />
-        <input
-          value={location}
-          onChange={handleInputChange}
-          className="placeholder:text-muted-foreground w-full py-2 focus:outline-none"
-          placeholder="What is your project size and location?"
-        ></input>
-      </div>
-      <Button onClick={sendMessage} className="text-lg">
-        Get Resources
-      </Button>
-      <div className="h-20">
-        {messages.map((message: string) => (
-          <p>{message}</p>
+    <main className="p-8 font-geist text-black min-h-dvh w-full flex flex-col justify-center items-center gap-8 container mx-auto">
+      <img className="w-32" src={WindSVG} />
+      <h1 className="text-3xl text-bold">Onshore Wind Development</h1>
+      <div className="space-y-5 max-w-xl w-full">
+        {messages.map((message: Message, id: number) => (
+          <div key={id}>
+            <div
+              className={`text-sm w-fit bg-slate-200 rounded-md p-3 ${message.type == MessageType.user ? 'ml-auto' : ''}`}
+            >
+              <p className="font-semibold">{message.type}</p>
+              <p className="mt-2">{message.content}</p>
+            </div>
+            {message.files != null ? (
+              <div className="mt-2">
+                {message.files.map((file: FileType) => (
+                  <div className="text-sm w-fit bg-slate-200 rounded-md p-3 flex flex-col justify-center items-center gap-2">
+                    <File width={20} height={20} />
+                    <a
+                      className="text-primary underline"
+                      href={`http://localhost:8000/download/${file.link}`}
+                    >
+                      {file.name}
+                    </a>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
         ))}
+      </div>
+      <div className="max-w-2xl items-center flex flex-col gap-4 w-full">
+        <Textarea
+          value={query}
+          onChange={handleInputChange}
+          className="border-black"
+          rows={1}
+        />
+        <Button className="w-36" onClick={sendMessage}>
+          Submit
+        </Button>
       </div>
     </main>
   );
--- server/main.py
+++ server/main.py
@@ -1,7 +1,10 @@
+from typing import List
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from lib import agent 
+from pathlib import Path
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -23,19 +26,36 @@ app.add_middleware(
 class QueryRequest(BaseModel):
     query: str
 
+
+class File(BaseModel):
+    name: str
+    link: str
+
 class QueryResponse(BaseModel):
-    output: str
-
-@app.post("/query_location", response_model=QueryResponse)
-async def query_location(request: QueryRequest):
-    return QueryResponse(output=request.query)
+    content: str
+    files: List[File]
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        async for text in agent.enviro_agent.query(data):
-            await websocket.send_text(text)
-        await websocket.send_text("[END]")
+sources = [
+    File(name="sr_snapping_turtle_0809_e.pdf", link="sr_snapping_turtle_0809_e.pdf")
+]
+
+@app.post("/query_agent", response_model=QueryResponse)
+async def query_agent(request: QueryRequest):
+    query = agent.enviro_agent.query(request.query)
+    return QueryResponse(content=query.response, files=sources)
+
+@app.get("/download/{filename}")
+async def download_file(filename: str):
+    file_path = Path("../data/raw/enviro-ns") / filename
+    if file_path.exists():
+        return FileResponse(file_path, media_type='application/octet-stream', filename=filename)
+    return {"error": "File not found"}
+
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+#     while True:
+#         data = await websocket.receive_text()
+#         query = agent.enviro_agent.query(data)
+#         await websocket.send_text(query.response)
